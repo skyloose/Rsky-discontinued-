91db40439240f7b045ea7f2709d50fb14b2effba
Noibr <134155765+Noibr@users.noreply.github.com>
2023-05-25 12:51:53 -0700

    Update main.lua

---
+++ SKYUI/Componentes/main.lua
@@ -0,0 +1,27 @@
+local placeid = game.PlaceId
+
+
+
+if placeid == 3182083477 then
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Noibr/Rsky/main/SKYUI/Componentes/Area51/3182083477.lua")()
+
+
+
+end
+
+if placeid == 2092166489 then
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Noibr/Rsky/main/SKYUI/Componentes/Area51/2092166489.lua")()
+
+
+
+end
+
+
+if placeid == 5740483929 then
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Noibr/Rsky/main/SKYUI/Componentes/Area51/5740483929.lua")()
+
+
+end
